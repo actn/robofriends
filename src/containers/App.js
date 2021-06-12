94fb80acc4a8a6ad4242003c179b37928d5dcfd9
actn <anilcetin@anadolu.edu.tr>
2021-06-12 12:48:51 +0300

    error boundry added

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       robots: [],
       searchfield: '',
@@ -21,23 +22,23 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
   render() {
-    const {robots,searchfield} = this.state;
+    const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    return!robots.length ? <h1>Loading</h1>  :
-     (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
             <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
   }
 }
 
